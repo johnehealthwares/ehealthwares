b872327791142781e8a910566dba867df68c2d7f
John <topzy20@gmail.com>
2026-07-14 21:49:50 +0100

    Update api url

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: process.env.API_URL ? `${process.env.API_URL}/api/:path*` : 'http://rxsoft-backend:8080/api/:path*',
+        source: '/:path*',
+        destination: process.env.API_URL ? `${process.env.API_URL}/:path*` : 'http://rxsoft-backend:8080/:path*',
       },
     ];
   },
